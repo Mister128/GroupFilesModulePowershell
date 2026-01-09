91d4d6e9b56ecc0df0bc8874abea1568a433d58e
MisterY <146533897+Mister128@users.noreply.github.com>
2026-01-09 11:37:49 +0300

    Sort is work

--- GroupFiles.ps1
+++ GroupFiles.ps1
@@ -26,42 +26,50 @@ function Group-Files {
         # init config files and check their exist
         $configCreateDirs = "./ConfCreateDirectories.json"
         $configSearchDirs = "./ConfSearchDirectories.json"
-        if (-not ((Test-Path $configCreateDirs) -or -not (Test-Path $configSearchDirs)) ) {
-            Write-Error "Config file(s) is not exist!"
-            exit 1
+        if (-not (Test-Path $configCreateDirs) -or -not (Test-Path $configSearchDirs)) {
+            Write-Error "One or both config files are missing!"
+            return
+        } else {
+            $configCreateDirs = Get-Content $configCreateDirs | ConvertFrom-Json
+            $configSearchDirs = Get-Content $configSearchDirs | ConvertFrom-Json
         }
 
+        $files = Get-ChildItem -File # all files in current location
+        $directories = Get-ChildItem -Directory | Where-Object { -not $_.Name.StartsWith('.') } # all directories in current location
+        foreach ($category in $configSearchDirs.SearchDirectories.PSObject.Properties) { 
+            $allowedExtentions = $category.Name -split "/"
+            $currentFiles = $files | Where-Object { $_.Extension -in $allowedExtentions } 
+            $currentDirectory = @($directories | Where-Object { $_.Name -in $category.Value })[0]
 
-        if (-not $Force) {
-            # json
-            $searchDirs = Get-Content $configSearchDirs | ConvertFrom-Json
-            
-            # all allowed names of directories and extentions of files
-            $allowedNames = @()
-            $allowedExtentions = @()
-            foreach ($category in $searchDirs.SearchDirectories.PSObject.Properties) {
-                $allowedNames += $category.Value
-                $allowedExtentions += $category.Name -split "/"
+            if (($null -eq $currentDirectory) -and $Force -and $currentFiles) {
+                Write-Verbose "We have '-Force' and don't have dir for $currentFiles, so we add this dir"
+                $currentDirectory = $configCreateDirs.PSObject.Properties[$category.Name].Value
+                mkdir $currentDirectory
             }
 
-            # current directories
-            $folders = Get-ChildItem -Directory | Where-Object {
-                -not $_.Name.StartsWith('.') -and
-                $_.Name.ToLower() -in $allowedNames
-            }            
+            if ($currentDirectory -and $currentFiles) {
+                Write-Verbose "Found directory and file(s). Processing $($currentFiles.Count) file(s)."
+    
+                foreach ($file in $currentFiles) {
+                    $destinationPath = Join-Path -LiteralPath $currentDirectory -ChildPath $file.Name
 
-            # current extentions
-            $extentions = Get-ChildItem -File | Where-Object {$_.Extension -in $allowedExtentions}  
-            
-            # проходимся по всем спискам json и к каждому подбираем свою директорию
-        } else {
-            
+                    if (Test-Path -LiteralPath $destinationPath) {
+                        if ($Force) {
+                            Write-Verbose "File '$destinationPath' exists. Overwriting."
+                            Move-Item -LiteralPath $file.FullName -Destination $destinationPath -Force
+                        } else {
+                            Write-Verbose "File '$destinationPath' already exists. Skipping (use -Force to overwrite)."
+                        }
+                    } else {
+                        Move-Item -LiteralPath $file.FullName -Destination $destinationPath
+                    }
+                }
+            }
         }
-        
     }
     
     end {
-        
+        Write-Verbose "Finish sort file(s)"
     }
 }
-Group-Files
+Group-Files -Force -Verbose
